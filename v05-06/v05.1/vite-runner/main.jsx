e5df1c6e1ad834fa9874741000d996474c1b228b
whoelse <prwabc@126.com>
2024-01-16 01:01:24 +0800

    v02

--- v05-06/v05.1/vite-runner/main.jsx
+++
@@ -1,4 +0,0 @@
-import ReactDom from "./core/ReactDom.js";
-import App from "./App.jsx";
-// ReactDom.createRoot(document.querySelector("#root")).render(<App></App>);
-ReactDom.createRoot(document.querySelector("#root")).render(App);
